29f5100cc2100ec4dffd48ed71ef3600fae1a3f1
shestyf <sofia.styf@masarinconsulting.se>
2022-05-06 16:40:04 +0200

    added better float comparison for checking if unix time is empty

--- internal/pkg/application/transform/transformers.go
+++ internal/pkg/application/transform/transformers.go
@@ -2,6 +2,7 @@ package transform
 
 import (
 	"context"
+	"math"
 	"time"
 
 	iotcore "github.com/diwise/iot-core/pkg/messaging/events"
@@ -16,7 +17,7 @@ func WeatherObserved(ctx context.Context, msg iotcore.MessageAccepted) (any, err
 	weatherObserved := fiware.NewWeatherObserved("", msg.Latitude(), msg.Longitude(), msg.Timestamp)
 	weatherObserved.Temperature = ngsi.NewNumberProperty(*msg.Pack[1].Value)
 
-	if msg.Pack[0].BaseTime != 0.0 {
+	if !almostEqual(msg.Pack[0].BaseTime, 0.0) {
 		t := parseTime(msg.Pack[0].BaseTime)
 		weatherObserved.DateObserved = *ngsi.CreateDateTimeProperty(t)
 	}
@@ -29,7 +30,7 @@ func WaterQualityObserved(ctx context.Context, msg iotcore.MessageAccepted) (any
 	waterQualityObserved := fiware.NewWaterQualityObserved("", msg.Latitude(), msg.Longitude(), msg.Timestamp)
 	waterQualityObserved.Temperature = ngsi.NewNumberProperty(*msg.Pack[1].Value)
 
-	if msg.Pack[0].BaseTime != 0.0 {
+	if !almostEqual(msg.Pack[0].BaseTime, 0.0) {
 		t := parseTime(msg.Pack[0].BaseTime)
 		waterQualityObserved.DateObserved = *ngsi.CreateDateTimeProperty(t)
 	}
@@ -42,7 +43,7 @@ func AirQualityObserved(ctx context.Context, msg iotcore.MessageAccepted) (any, 
 	airQualityObserved := fiware.NewAirQualityObserved("", 0.0, 0.0, msg.Timestamp)
 	airQualityObserved.CO2 = ngsi.NewNumberProperty(*msg.Pack[1].Value)
 
-	if msg.Pack[0].BaseTime != 0.0 {
+	if !almostEqual(msg.Pack[0].BaseTime, 0.0) {
 		t := parseTime(msg.Pack[0].BaseTime)
 		airQualityObserved.DateObserved = *ngsi.NewTextProperty(t)
 	}
@@ -56,4 +57,10 @@ func parseTime(unixTime float64) string {
 	t := time.Unix(n, 0)
 
 	return t.UTC().Format(time.RFC3339)
+}
+
+const float64EqualityThreshold = 1e-9
+
+func almostEqual(a, b float64) bool {
+	return math.Abs(a-b) <= float64EqualityThreshold
 }
