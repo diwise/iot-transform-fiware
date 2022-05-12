06111a1d234638dc9b1e05c9523eb0029166eef3
shestyf <sofia.styf@masarinconsulting.se>
2022-05-12 15:41:32 +0200

    added missing env to airquality type in transformer registry

--- internal/pkg/application/transform/transformerregistry.go
+++ internal/pkg/application/transform/transformerregistry.go
@@ -17,7 +17,7 @@ func NewTransformerRegistry() TransformerRegistry {
 		"urn:oma:lwm2m:ext:3303/water":   WaterQualityObserved,
 		"urn:oma:lwm2m:ext:3303/air":     WeatherObserved,
 		"urn:oma:lwm2m:ext:3303/indoors": AirQualityObserved,
-		"urn:oma:lwm2m:ext:3428":         AirQualityObserved,
+		"urn:oma:lwm2m:ext:3428/indoors": AirQualityObserved,
 	}
 
 	return &transformerRegistry{
