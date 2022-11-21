3ff42bf94df9ec7f6189665002a606201aa0f071
shestyf <sofia.styf@masarinconsulting.se>
2022-11-21 17:37:05 +0100

    adding indoorenvironmentobserved...

--- internal/pkg/application/transform/transformerregistry.go
+++ internal/pkg/application/transform/transformerregistry.go
@@ -16,8 +16,9 @@ func NewTransformerRegistry() TransformerRegistry {
 	transformers := map[string]MessageTransformerFunc{
 		"urn:oma:lwm2m:ext:3303/water":    WaterQualityObserved,
 		"urn:oma:lwm2m:ext:3303/air":      WeatherObserved,
-		"urn:oma:lwm2m:ext:3303/indoors":  AirQualityObserved,
-		"urn:oma:lwm2m:ext:3428/indoors":  AirQualityObserved,
+		"urn:oma:lwm2m:ext:3303/indoors":  IndoorEnvironmentObserved,
+		"urn:oma:lwm2m:ext:3428/indoors":  IndoorEnvironmentObserved,
+		"urn:oma:lwm2m:ext:3428":          AirQualityObserved,
 		"urn:oma:lwm2m:ext:3302":          Device,
 		"urn:oma:lwm2m:ext:3302/lifebuoy": Lifebuoy,
 		"urn:oma:lwm2m:ext:3424":          WaterConsumptionObserved,
