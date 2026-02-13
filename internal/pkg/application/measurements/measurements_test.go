4bdb3f0e6106fde051af3c76b327250add29f3d7
Mikael Nordberg <mikael.nordberg@outlook.com>
2026-02-13 14:59:12 +0100

    merge först

--- internal/pkg/application/measurements/measurements_test.go
+++ internal/pkg/application/measurements/measurements_test.go
@@ -58,7 +58,7 @@ func TestThatAirQualityObservedCanBeCreated(t *testing.T) {
 	err := AirQualityObserved(context.Background(), *msg, cbClient)
 
 	is.NoErr(err)
-	is.Equal(len(cbClient.MergeEntityCalls()), 0)
+	is.Equal(len(cbClient.MergeEntityCalls()), 1)
 
 	b, _ := json.Marshal(cbClient.CreateEntityCalls()[0].Entity)
 	is.True(strings.Contains(string(b), `"CO2":{"type":"Property","value":22.2,"observedAt":"2022-01-01T00:00:00Z"}`))
@@ -176,7 +176,7 @@ func TestThatIndoorEnvironmentObservedCanBeCreated(t *testing.T) {
 
 	err := IndoorEnvironmentObserved(context.Background(), *msg, cbClient)
 	is.NoErr(err)
-	is.Equal(len(cbClient.MergeEntityCalls()), 0)
+	is.Equal(len(cbClient.MergeEntityCalls()), 1)
 
 	b, _ := json.Marshal(cbClient.CreateEntityCalls()[0].Entity)
 	is.True(strings.Contains(string(b), `"temperature":{"type":"Property","value":22.2,"observedAt":"2022-01-01T00:00:00Z"},"type":"IndoorEnvironmentObserved"}`))
@@ -213,7 +213,7 @@ func TestThatWaterConsumptionObservedIsPatchedIfAlreadyExisting(t *testing.T) {
 			return &ngsild.UpdateEntityAttributesResult{}, nil
 		},
 		MergeEntityFunc: func(ctx context.Context, entityID string, fragment types.EntityFragment, headers map[string][]string) (*ngsild.MergeEntityResult, error) {
-			return &ngsild.MergeEntityResult{}, nil
+			return nil, fmt.Errorf("not found")
 		},
 		RetrieveEntityFunc: func(ctx context.Context, entityID string, headers map[string][]string) (types.Entity, error) {
 			return nil, fmt.Errorf("not found")
@@ -226,7 +226,7 @@ func TestThatWaterConsumptionObservedIsPatchedIfAlreadyExisting(t *testing.T) {
 	err := WaterConsumptionObserved(context.Background(), *msg, cbClient)
 	is.NoErr(err)
 
-	is.Equal(len(cbClient.CreateEntityCalls()), 1) // update entity attributes should have been called once
+	is.Equal(len(cbClient.CreateEntityCalls()), 1) // create entity should have been called once
 
 	expectedEntityID := "urn:ngsi-ld:WaterConsumptionObserved:watermeter-01"
 	is.Equal(cbClient.CreateEntityCalls()[0].Entity.ID(), expectedEntityID) // the entity id should be ...
@@ -298,7 +298,7 @@ func TestThatWeatherObservedCanBeCreated(t *testing.T) {
 	err := WeatherObserved(context.Background(), *msg, cbClient)
 	is.NoErr(err)
 
-	is.Equal(len(cbClient.MergeEntityCalls()), 0)
+	is.Equal(len(cbClient.MergeEntityCalls()), 1)
 
 	b, _ := json.Marshal(cbClient.CreateEntityCalls()[0].Entity)
 	is.True(strings.Contains(string(b), `"temperature":{"type":"Property","value":22.2,"observedAt":"2022-01-01T00:00:00Z"},"type":"WeatherObserved"`))
